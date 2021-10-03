4334617e16a6e9132bd5ffd285760169c25633ca
thuwarakeshm <thuwarakeshm@stax.com>
2021-10-03 12:42:44 +0530

    Read config values from pyroject.toml

--- blueprint/main.py
+++ blueprint/main.py
@@ -1,3 +1,8 @@
+import toml
+
+APP_CONFIG = toml.load("pyproject.toml")["app"]
+
+
 def app():
     """This is the main entrypoint to your application"""
-    print("Let's build some cool python apps!")
+    print(f"This is {APP_CONFIG['APP_NAME']}. Let's build some cool python apps!")
